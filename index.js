3b3cad0d446fa727412ea253aba68ec970034691
C K Boyt <ckboyt@gmail.com>
2023-03-01 16:12:36 -0500

    Add full JS functionality

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project?',
+    'Please provide a brief description of your project.',
+    'What are the steps required to install your project?',
+    'What are the steps required to use your project?',
+    'Please provide the URL or path to a screenshot of your project.',
+    'How should users report issues?',
+    'How can users contibute to the project?',
+    'Please list any credits for your project, ex. collaborators, third-party assets, or tutorials.',
+    'Which license is your project using?',
+    'What would you like the filename to be? Do not include the extension.'
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    markdown = generateMarkdown(data);
+    fs.writeFile('./output/' + fileName, markdown, (err) =>
+        err ? console.log(err) : console.log(fileName + ' successfully created in the "output" directory.')
+    );
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'desc',
+                message: questions[1],
+            },
+            {
+                type: 'editor',
+                name: 'install',
+                message: questions[2],
+            },
+            {
+                type: 'editor',
+                name: 'usage',
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: 'screenshot',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'issues',
+                message: questions[5],
+            },
+            {
+                type: 'input',
+                name: 'contribute',
+                message: questions[6],
+            },
+            {
+                type: 'editor',
+                name: 'credits',
+                message: questions[7],
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[8],
+                choices: [
+                    'NONE',
+                    'Apache License 2.0',
+                    'Boost Software License 1.0',
+                    'GNU AGPLv3',
+                    'GNU GPLv3',
+                    'GNU LGPLv3',
+                    'ISC',
+                    'MIT',
+                    'Mozilla Public License 2.0',
+                    'The Unlicense'
+                ],
+            },
+            {
+                type: 'input',
+                name: 'filename',
+                message: questions[9],
+            },
+        ])
+        .then((data) => {
+            writeToFile(data.filename + '.md', data);
+        })
+}
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,100 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseLinks = {
+  'Apache License 2.0': {
+    badge: 'Apache_2.0-blue.svg',
+    link: 'https://opensource.org/licenses/Apache-2.0'
+  },
+  'Boost Software License 1.0': {
+    badge: 'Boost_1.0-lightblue.svg',
+    link: 'https://www.boost.org/LICENSE_1_0.txt'
+  },
+  'GNU AGPLv3': {
+    badge: 'AGPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/agpl-3.0'
+  },
+  'GNU GPLv3': {
+    badge: 'GPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/gpl-3.0'
+  },
+  'GNU LGPLv3': {
+    badge: 'LGPL_v3-blue.svg',
+    link: 'https://www.gnu.org/licenses/lgpl-3.0'
+  },
+  'ISC': {
+    badge: 'ISC-blue.svg',
+    link: 'https://opensource.org/licenses/ISC'
+    
+  },
+  'MIT': {
+    badge: 'MIT-yellow.svg',
+    link: 'https://opensource.org/licenses/MIT'
+  },
+  'Mozilla Public License 2.0': {
+    badge: 'MPL_2.0-brightgreen.svg',
+    link: 'https://opensource.org/licenses/MPL-2.0'
+  },
+  'The Unlicense': {
+    badge: 'Unlicense-blue.svg',
+    link: 'http://unlicense.org/'
+  }
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'NONE') {
+    return `https://img.shields.io/badge/License-${licenseLinks[license].badge}`;
+  } else {
+    return '';
+  };
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'NONE') {
+    return licenseLinks[license].link;
+  } else {
+    return '';
+  };
+};
 
-// TODO: Create a function to generate markdown for README
+function renderLicenseSection(license) {
+  if (license !== 'NONE') {
+    return `
+## License
+
+[![License: ${license}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`;
+  } else {
+    return '';
+  };
+};
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${data.desc}
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Screenshot
+
+![Screenshot](${data.screenshot})
+
+## Reporting Issues
+
+${data.issues}
+
+## Contributing
+
+${data.contribute}
+
+## Credits
+
+${data.credits}
+${renderLicenseSection(data.license)}
 `;
-}
+};
 
 module.exports = generateMarkdown;
